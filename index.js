c83172c62752806ec1f5af5d4cd83e0cb8f53e6c
Eric Kollegger <eric.kollegger@gmail.com>
2017-10-19 13:00:53 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function theBeatlesPlay(musicians, instruments) {
+  var beatleFacts = [];
+  for (var i = 0; i < musicians.length; i++) {
+    beatleFacts.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return beatleFacts
+}
+
+function johnLennonFacts(facts) {
+  var l = facts.length;
+  var i = 0;
+  while (i < l) {
+    i++;
+    facts[i] = `${facts[i]}!!!`;
+  }
+  return facts
+}
